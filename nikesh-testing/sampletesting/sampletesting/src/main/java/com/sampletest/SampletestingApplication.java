c2f780bc4a1e346644edab842672b2028ca28d7b
saini <saini@LAPTOP-0HCMV8K5.mshome.net>
2022-06-20 21:46:45 +0530

    testing files

---
+++ nikesh-testing/sampletesting/sampletesting/src/main/java/com/sampletest/SampletestingApplication.java
@@ -0,0 +1,13 @@
+package com.sampletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampletestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampletestingApplication.class, args);
+	}
+
+}
